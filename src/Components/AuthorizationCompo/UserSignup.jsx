204ba74ca43dcb8a6d9684090c8e4c59e714d5a6
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-05-03 20:15:57 +0600

    sign in and sign up page ready

--- src/Components/AuthorizationCompo/UserSignup.jsx
+++ src/Components/AuthorizationCompo/UserSignup.jsx
@@ -1,12 +1,16 @@
 
-const UserLogin = () => {
+const UserSignup = () => {
     const handleLogin = e => {
         e.preventDefault();
         const form = e.target;
+        const name = form.name.value;
         const email = form.email.value;
+        const phone = form.phone.value;
         const password = form.password.value;
         const user = {
+            name: name,
             email: email,
+            phone: phone,
             password: password,
         }
         console.log(user);
@@ -15,16 +19,30 @@ const UserLogin = () => {
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
                 <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <h1 className="text-5xl font-bold">Sign in now!</h1>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleLogin} className="card-body">
+                        {/* User Name */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="Name" name="name" className="input input-bordered" required />
+                        </div>
                         {/* User Email */}
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        </div>
+                        {/* User Phone number */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Phone</span>
+                            </label>
+                            <input type="tel" placeholder="Phone" name="phone" className="input input-bordered" required />
                         </div>
                         {/* Password */}
                         <div className="form-control">
@@ -46,4 +64,4 @@ const UserLogin = () => {
     );
 };
 
-export default UserLogin;
+export default UserSignup;
