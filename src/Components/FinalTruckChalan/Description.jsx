41769420ee2891f284512ccadbb1edba38a59cc5
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-05-18 20:05:02 +0600

    final chalan done

--- src/Components/FinalTruckChalan/Description.jsx
+++ src/Components/FinalTruckChalan/Description.jsx
@@ -1,15 +1,28 @@
 const Description = () => {
     return (
-        <div>
+        <div className="">
             <div className="mx-5 mt-5 p-6 border-2 border-green-500 rounded-lg grid grid-cols-4 justify-center">
                 <div className="border-2 col-span-2 p-4 text-center">
                     <h4 className="">Product Description:</h4>
                     <h3 className="text-start">Imported Carbon Block</h3>
                 </div>
-                <h4 className="border-2 col-span-1 p-4 text-center">Quantity</h4>
-                <h4 className="border-2 col-span-1 p-4 text-center">Truck Fare</h4>
-                {/* <h4 className="border-2">Advance:</h4>
-                <h4 className="border-2">Net Pay:</h4> */}
+                <div className="border-2 col-span-1 p-4 text-center">
+                    <h4>Quantity</h4>
+                    <h3>14,000KG</h3>
+                </div>
+
+                <div className="border-2 col-span-1 p-4 text-center">
+                    <h4>Truck Fare</h4>
+                    <h3 className="text-end">31,000</h3>
+                    <div className="grid grid-cols-3">
+                        <h2 className="col-span-2" >Advance:</h2>
+                        <h2 className="col-span-1 text-end">12000</h2>
+                    </div>
+                    <div className="grid grid-cols-3">
+                        <h2 className="col-span-2" >Net Fare:</h2>
+                        <h2 className="col-span-1 text-end">12000</h2>
+                    </div>
+                </div>
             </div>
         </div>
     );
