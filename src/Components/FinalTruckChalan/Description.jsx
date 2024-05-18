ca48146e84c93e59a6107c462176faf5bc550a76
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-05-18 13:52:29 +0600

    working on final chalan description

--- src/Components/FinalTruckChalan/Description.jsx
+++ src/Components/FinalTruckChalan/Description.jsx
@@ -2,9 +2,12 @@ const Description = () => {
     return (
         <div>
             <div className="mx-5 mt-5 p-6 border-2 border-green-500 rounded-lg grid grid-cols-4 justify-center">
-                <h4 className="border-2 col-span-2 px-4 pt-4 text-center">Product Description:</h4>
-                <h4 className="border-2">Quantity</h4>
-                <h4 className="border-2">Truck Fare: </h4>
+                <div className="border-2 col-span-2 p-4 text-center">
+                    <h4 className="">Product Description:</h4>
+                    <h3 className="text-start">Imported Carbon Block</h3>
+                </div>
+                <h4 className="border-2 col-span-1 p-4 text-center">Quantity</h4>
+                <h4 className="border-2 col-span-1 p-4 text-center">Truck Fare</h4>
                 {/* <h4 className="border-2">Advance:</h4>
                 <h4 className="border-2">Net Pay:</h4> */}
             </div>
